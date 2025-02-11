f1f83f6b493b03789f04c50b16c95263337ea275
Rony <rniumo@gmail.com>
2025-02-11 12:12:03 +0600

    fix(po): remove the on_delete parameter

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    rfp_id = fields.Many2one('supplies.rfp', string='RFP', ondelete='cascade', index=True, copy=False)
+    rfp_id = fields.Many2one('supplies.rfp', string='RFP', index=True, copy=False)
     warrenty_period = fields.Integer(string='Warrenty Period (in months)')
     score = fields.Float(string='Score', default=0)
     recommended = fields.Boolean(string='Recommended', default=False)

ce637dd44b0228e4571cffdd9db86824b391e1f9
Rony <rniumo@gmail.com>
2025-02-16 16:41:52 +0600

    feat: add supplier selection and period filter to supplies dashboard

--- static/src/components/supplies_dashboard.js
+++ static/src/components/supplies_dashboard.js
@@ -10,6 +10,8 @@ export class SuppliesDashboard extends Component {
         this.state = useState({
             suppliers: [],
             selectedSupplierId: null,
+            selectedPeriod: "0",
+            selectedPeriodDate: null,
             rfp: {
                 'accepted': 0,
                 'submitted': 0,
@@ -20,8 +22,24 @@ export class SuppliesDashboard extends Component {
 
         onWillStart(async () => {
             await this.getSuppliers();
-            await this.getRequestForPurchases();
         });
+    }
+
+    onChangeFilter() {
+        this.setValues();
+        this.getRequestForPurchases();
+    }
+
+    setValues() {
+        if (this.state.selectedSupplierId === "0") {
+            this.state.selectedSupplierId = null;
+        } else {
+            this.state.selectedSupplierId = parseInt(this.state.selectedSupplierId);
+        }
+        const today = new Date();
+        const days = parseInt(this.state.selectedPeriod);
+        today.setDate(today.getDate() - days);
+        this.state.selectedPeriodDate = today.toISOString().split('T')[0];
     }
 
     async getSuppliers() {
@@ -30,7 +48,17 @@ export class SuppliesDashboard extends Component {
     }
 
     async getRequestForPurchases() {
-        const rfps = await this.orm.searchRead('supplies.rfp', [], ['state', 'total_amount']);
+        const domain = [];
+        if (this.state.selectedSupplierId) {
+            domain.push(['approved_supplier_id', '=', this.state.selectedSupplierId]);
+        } else {
+            return;
+        }
+        if (this.state.selectedPeriod !== "0") {
+            domain.push(['create_date', '>=', this.state.selectedPeriodDate]);
+        }
+
+        const rfps = await this.orm.searchRead('supplies.rfp', domain, ['state', 'total_amount']);
         const accepted_rfps = rfps.filter(r => r.state === 'accepted');
         const submitted = rfps.length;
         const accepted = accepted_rfps.length;
